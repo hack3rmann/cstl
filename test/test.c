cbc840b73a01622e8b57726d08e0c49c7acdfb33
Svyatoslav <slavavarov30727@gmail.com>
2023-11-10 23:24:22 +0300

    `README.md` added to repo.

--- test/test.c
+++ test/test.c
@@ -3,10 +3,14 @@
 #include "../prelude.h"
 #include "../iterator.h"
 #include "../format.h"
+#include "../memory.h"
 
 
 
 i32 main(void) {
+    AddrMut const ptr = mem_alloc(99999999999999999);
+
+    assert_fmt(null_mut != ptr, "ptr = {Addr} is invalid pointer value", ptr);
 
     return EXIT_SUCCESS;
 }
